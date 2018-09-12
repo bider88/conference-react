7b2c94dc10df1090bb95cc598baf5411668c8872
Irving Didier <bider88@gmail.com>
2018-09-12 11:50:51 -0500

    Agregando un header

---
+++ src/common/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Functional stateless react component
+const Header = ( props ) => {
+    return (
+        <h1>{ props.title }</h1>
+    )
+}
+
+export default Header;
