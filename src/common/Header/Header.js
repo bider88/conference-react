385d1fb4b947034967e3375234013bf8b2b6162f
Irving Didier <bider88@gmail.com>
2018-09-12 22:31:42 -0500

    Agregando bootstrap y modificando el header

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import "./Header.css";
 
 // Functional stateless react component
-const Header = ( props ) => {
-    return (
-        <h1>{ props.title }</h1>
-    )
-}
+const Header = (props) => {
+	return (
+		<nav className="navbar navbar-expand-md navbar-light navbar-laravel b-navigation-bar">
+			<div className="container">
+				<a className="navbar-brand" href="/">
+					Conference
+            </a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item">
+							<a href="/program" className="b-nav-link">
+								Programa
+              </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
 export default Header;
