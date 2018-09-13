72c2b24d3bbed62bb828e3e2a7cd5344d1a2728d
Irving Didier <bider88@gmail.com>
2018-09-12 23:32:27 -0500

    Renderizando los datos del backend en ProgramAgenda

--- src/Program/ProgramAgenda.js
+++ src/Program/ProgramAgenda.js
@@ -3,23 +3,95 @@ import React from 'react';
 // Stateful react component (class)
 export default class ProgramAgenda extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      talks: []
+    }
+  }
+
   componentDidMount() {
     console.log('componentDidMount');
 
     const url = 'http://conference.webtraining.fun/api/talks';
 
-    fetch( url )
-    .then( response => response.json() )
-    .then( result => {
-      console.log(result);
-    })
-    .catch( err => console.log(err) );
+    fetch(url)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+
+        this.setState({
+          isLoaded: true,
+          talks: result
+        });
+
+      })
+      .catch(err => console.log(err));
 
   }
 
   render() {
+
+    const { talks } = this.state;
+
     return <div>
-      Agenda
+
+      <section className="b-section b-section--news mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h2>
+                Agenda
+              </h2>
+
+              {
+                talks.map(talk =>
+
+                  <article className="b-schedule-item" key={talk.id}>
+
+                    <div className="b-schedule-item-header__date">
+                      <span className="b-schedule-item-header__time">
+                        Hora
+                    </span>
+                    </div>
+
+                    <div className="b-schedule-item-body">
+                      <div className="row">
+                        <div className="col-12 col-sm-7 col-md-5 col-lg-4 col-xl-4">
+                          <img src={talk.thumbnail} alt={talk.speaker.name} className="img-fluid rounded" />
+                        </div>
+                        <div className="col-12 col-sm-7 col-md-7 col-lg-8 col-xl-8">
+                          <span className="b-schedule-item__place text-uppercase">{talk.room.name}</span>
+                          <a href="#">
+                            <h4 className="b-schedule-item__title">
+                              {talk.title}
+                            </h4>
+                          </a>
+
+                          <p>
+                            <strong>Conferencista: </strong>
+                            <span className="b-schedule-item__speaker">{talk.speaker.name}</span>
+                          </p>
+
+                          <p className="b-schedule-item__summary">
+                            {talk.summary}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+
+                )
+              }
+
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   }
 }
